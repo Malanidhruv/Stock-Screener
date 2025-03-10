6d9deded60db44d561f479c4d5a49870f55322fa
Dhruv Malani <118156335+Malanidhruv@users.noreply.github.com>
2025-03-10 20:49:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 def get_user_credentials():
     """Return hardcoded credentials."""
-    return "1141826", "aAAnHSSZOWdPIpsZZeJ9W2C3aNEzqhZyiGsKqxeXbG5dvk4h84lgM6BbsWNKo6Dd8flz878hYSghz1YDT7KGZ0SsGIYOqv4YbbHZ7Wa1r0bXoA98z3yKXAkmAcCqLHdg"
+    return "1141826", "R6Qv5c3VmA4AJfKT3WSR1Byujn2nlfjK0de0hEKMCqcJBzrPB9EKHu010HRfctldIoRbRqY43uo5OLY0FE3W6qRuOLzyQZK4oyW5ngZWHGPAAuS0bqyuKsHQ7vu06udM"
